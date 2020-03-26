f313a0c88a40b01367137bf1638cf5e75dceb3bd
KHKS <kirill.khrabrov@yandex.ru>
2020-03-26 14:28:11 +0300

    Start VUEX

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import store from './store/index.js'
 import App from './App'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
+  store: store,
   el: '#app',
-    render: h => h(App)
+  render: h => h(App)
 })
